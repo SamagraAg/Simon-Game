a10b825f69e61d5d18e7e09ee3102bcc152999a0
Samagra Agrawal <114285524+SamagraAg@users.noreply.github.com>
2023-01-13 23:07:52 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,67 @@
+
+//defining audio variables
 var audio = new Audio('sounds/blue.mp3');
 var audio2 = new Audio('sounds/red.mp3');
 var audio3 = new Audio('sounds/yellow.mp3');
 var audio4 = new Audio('sounds/green.mp3');
+var wrongAns = new Audio('sounds/wrong.mp3');
 
-
-
-
+var level = 0;
+var FirstTime = true;
 var btnColors = ["green", "red", "yellow", "blue"]
 var gamePattern = [];
+var userClickedPattern = [];
+
+//Function to Restart the game after failing
+function startOver()
+{
+    level = 0;
+    gamePattern = [];
+    FirstTime = true;
+}
+
+//Function for checking the answer of user
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                newSequence();
+            }, 1000);
+        }
+    }
+    else {
+        wrongAns.play();
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h2").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+
+}
+
+//Event Listener for starting the game
+$(document).keydown(function () {
+    if (FirstTime) {
+        $("h2").text("level " + level);
+        newSequence();
+    }
+    FirstTime = false;
+});
+
+//Event listener for clicking of buttons
+$(".btn").click(function (e) {
+    var UserChosenColor = this.id;
+    userClickedPattern.push(UserChosenColor);
+    playSound(UserChosenColor);
+    animation(UserChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
 
 function newSequence() {
+    //reseting the userClickedPattern every time this function is called
+    userClickedPattern = [];
 
     //generating random number between 0-3
     var randomNumber = Math.floor(((Math.random()) * 4));
@@ -24,7 +76,8 @@ function newSequence() {
     $(mString).fadeOut(100);
     $(mString).fadeIn(100);
     playSound(randomColor);
-
+    level++;
+    $("h2").text("level " + level);
 }
 
 function playSound(color) {
@@ -50,7 +103,16 @@ function playSound(color) {
             break;
     }
 }
-newSequence();
+
+function animation(color) {
+    var id = "#" + color;
+    $(id).addClass("pressed");
+    function removeAnimation() {
+        $(id).removeClass("pressed");
+    }
+    setTimeout(removeAnimation, 100);
+}
+
 
 
 
